3e067251c10feb9d598cbfef18fa720f27b7cbd9
burzomir <burzomirdev@gmail.com>
2018-03-25 22:53:00 +0200

    create mock data

---
+++ src/data.ts
@@ -0,0 +1,38 @@
+export const data = {
+  timeline: {
+    sections: [1, 2]
+  },
+
+  sections: [{
+    id: 1,
+    name: 'Experience',
+    subsections: [1, 2, 3]
+  }, {
+    id: 2,
+    name: 'Education',
+    subsections: [4]
+  }],
+
+  subsections: [{
+    id: 1,
+    name: 'Bitcraft',
+    entries: [1]
+  }, {
+    id: 2,
+    name: 'YNDO',
+    entries: [1]
+  }, {
+    id: 3,
+    name: 'Freelancer',
+    entries: [1]
+  }, {
+    id: 4,
+    name: 'Bialystok University of Technology',
+    entries: [1]
+  }],
+
+  entries: [{
+    id: 1,
+    name: 'Front-end Developer'
+  }]
+}
