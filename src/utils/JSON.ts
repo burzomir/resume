b2a46d1742c6d19faefc9e3d8f7302a7b9b46b50
burzomir <burzomirdev@gmail.com>
2021-08-22 17:46:20 +0200

    Extract file upload

--- src/utils/JSON.ts
+++ src/utils/JSON.ts
@@ -1,16 +1,7 @@
-export function upload(): Promise<any> {
-  return new Promise((resolve, reject) => {
-    const input: HTMLInputElement = document.createElement("input");
-    input.type = "file";
-    input.multiple = false;
-    input.addEventListener("change", async (e) => {
-      if (e.target instanceof HTMLInputElement) {
-        const [file] = e.target.files;
-        const rawData = await file.text();
-        const parsedData = JSON.parse(rawData);
-        resolve(parsedData);
-      }
-    });
-    input.click();
-  });
+import * as FileUtils from "./file";
+
+export async function upload(): Promise<any> {
+  const file = await FileUtils.upload();
+  const text = await file.text();
+  return JSON.parse(text);
 }
---
+++ src/utils/file.ts
@@ -0,0 +1,14 @@
+export function upload(): Promise<File> {
+  return new Promise((resolve, reject) => {
+    const input: HTMLInputElement = document.createElement("input");
+    input.type = "file";
+    input.multiple = false;
+    input.addEventListener("change", async (e) => {
+      if (e.target instanceof HTMLInputElement) {
+        const [file] = e.target.files;
+        resolve(file);
+      }
+    });
+    input.click();
+  });
+}
