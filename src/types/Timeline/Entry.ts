c0be0a41ebe6c9c9df822630c6e88ef4e94dedf6
burzomir <burzomirdev@gmail.com>
2021-08-22 20:31:53 +0200

    Fill entry and section implementations

--- src/types/Timeline/Entry.ts
+++ src/types/Timeline/Entry.ts
@@ -2,11 +2,36 @@ export type Entry = {
   name: string;
   companyName: string;
   started: Date;
-  ended?: Date;
+  ended: Date | undefined;
 };
 
 export const defaultEntry: Entry = {
   name: "Entry",
   companyName: "Place",
   started: new Date(),
+  ended: undefined,
 };
+
+export function setName(name: string, entry: Entry): Entry {
+  const newEntry = { ...entry };
+  newEntry.name = name;
+  return newEntry;
+}
+
+export function setCompanyName(name: string, entry: Entry): Entry {
+  const newEntry = { ...entry };
+  newEntry.companyName = name;
+  return newEntry;
+}
+
+export function setStarted(date: Date, entry: Entry): Entry {
+  const newEntry = { ...entry };
+  newEntry.started = date;
+  return newEntry;
+}
+
+export function setEnded(date: Date | undefined, entry: Entry): Entry {
+  const newEntry = { ...entry };
+  newEntry.ended = date;
+  return newEntry;
+}
--- src/types/Timeline/Section.ts
+++ src/types/Timeline/Section.ts
@@ -5,7 +5,34 @@ export type Section = {
   entries: Entry[];
 };
 
-export const defaultSection: Section = {
-  name: "Section",
-  entries: [defaultEntry, defaultEntry],
-};
+export const defaultSection: Section = addEntry(
+  addEntry({
+    name: "Section",
+    entries: [],
+  })
+);
+
+export function addEntry(section: Section): Section {
+  const newSection = { ...section };
+  newSection.entries = [...section.entries, defaultEntry];
+  return newSection;
+}
+
+export function updateEntry(
+  index: number,
+  entry: Entry,
+  section: Section
+): Section {
+  const newSection = { ...section };
+  newSection.entries = section.entries.map((e, i) => (i === index ? entry : e));
+  return newSection;
+}
+
+export function removeEntry(index: number, section: Section): Section {
+  const newSection = { ...section };
+  newSection.entries = section.entries.reduce(
+    (es, e, i) => (i === index ? [...es, e] : es),
+    []
+  );
+  return newSection;
+}
