00a817b29e0a5abaa43ac2ad9ab494ed300dd72f
burzomir <burzomirdev@gmail.com>
2021-08-22 18:20:12 +0200

    Recreate page structure

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AvatarEditor } from "../components/AvatarEditor";
+import Page from "../components/Page";
 import * as Data from "../types/Data";
 import { Picture } from "../types/Picture";
 
@@ -22,16 +23,30 @@ function Editor() {
   };
 
   return (
-    <div>
-      <button onClick={upload}>Upload</button>
-      <button onClick={download}>Download</button>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      <AvatarEditor
-        picture={data.avatar}
-        onPictureChange={setPicture}
-        style={{ width: "200px", height: "200px" }}
-      />
-    </div>
+    <>
+      <div>
+        <button onClick={upload}>Upload</button>
+        <button onClick={download}>Download</button>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+      <div className="app">
+        <Page format="a4" className="d-f">
+          <div className="d-f p-2">
+            <div className="w-30 pr-2">
+              <div className="text-center mb-2">
+                <AvatarEditor
+                  picture={data.avatar}
+                  onPictureChange={setPicture}
+                  className="w-80"
+                />
+                <h1>User Name</h1>
+              </div>
+            </div>
+            <div className="w-70 d-f"></div>
+          </div>
+        </Page>
+      </div>
+    </>
   );
 }
 
