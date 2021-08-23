de0c8c93163c35dafe1068d85e823ac55c945fe7
burzomir <burzomirdev@gmail.com>
2021-08-23 17:01:17 +0200

    Add functions for managing sections

--- src/types/Skills/Sidebar.ts
+++ src/types/Skills/Sidebar.ts
@@ -1,4 +1,6 @@
 import { defaultSection, Section } from "./Section";
+import * as A from "../../utils/array";
+import * as O from "../../utils/object";
 
 export type Sidebar = {
   sections: Section[];
@@ -7,3 +9,22 @@ export type Sidebar = {
 export const defaultSidebar: Sidebar = {
   sections: [defaultSection],
 };
+
+export function addSection(sidebar: Sidebar): Sidebar {
+  const sections = A.add(defaultSection, sidebar.sections);
+  return O.set("sections", sections, sidebar);
+}
+
+export function updateSection(
+  index: number,
+  section: Section,
+  sidebar: Sidebar
+): Sidebar {
+  const sections = A.update(index, section, sidebar.sections);
+  return O.set("sections", sections, sidebar);
+}
+
+export function removeSection(index: number, sidebar: Sidebar): Sidebar {
+  const sections = A.remove(index, sidebar.sections);
+  return O.set("sections", sections, sidebar);
+}
